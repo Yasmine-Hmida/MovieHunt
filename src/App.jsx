d2ed8df3a091fb1b0b5aa8032158979cdeadc4d4
Yasmine-Hmida <hmidayasmiine@gmail.com>
2025-07-31 09:58:43 +0100

    Creation of the MovieCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import MovieCard from "./components/MovieCard"
 
 function App() {
   return (
     <div>
-          
+        <MovieCard movie={{title : "Yasso's movie" , release_date: "2024" }}/>
+        <MovieCard movie={{title : "Joes movie" , release_date: "2020" }}/>
     </div>
   )
 }
